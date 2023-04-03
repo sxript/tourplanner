c4d64836b36f6414268652a19e9109f338d034a7
Marko Pavlovic <markopavlovic205051@gmail.com>
2023-04-03 17:11:15 +0200

    change controller and views to tourLog and add list for tourList

---
+++ swen2-template/src/main/java/at/fhtw/swen2/tutorial/presentation/view/TourListController.java
@@ -0,0 +1,47 @@
+package at.fhtw.swen2.tutorial.presentation.view;
+
+import at.fhtw.swen2.tutorial.presentation.viewmodel.TourLogListViewModel;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Component
+public class TourListController implements Initializable{
+
+    @Autowired
+    public TourLogListViewModel tourLogListViewModel;
+
+    @FXML
+    public TableView tableView = new TableView<>();
+    @FXML
+    private VBox dataContainer;
+
+    @Override
+    public void initialize(URL location, ResourceBundle rb){
+        tableView.setItems(tourLogListViewModel.getPersonListItems());
+        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn date = new TableColumn("Date");
+        date.setCellValueFactory(new PropertyValueFactory("date"));
+
+        TableColumn duration = new TableColumn("Duration");
+        duration.setCellValueFactory(new PropertyValueFactory("duration"));
+
+        TableColumn distance = new TableColumn("Distance");
+        distance.setCellValueFactory(new PropertyValueFactory("distance"));
+
+        tableView.getColumns().addAll(date, duration, distance);
+
+        dataContainer.getChildren().add(tableView);
+        tourLogListViewModel.initList();
+    }
+
+}

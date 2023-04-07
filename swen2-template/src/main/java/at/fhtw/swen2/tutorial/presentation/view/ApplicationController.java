d4072274f02cf7f19a80f67dcca25abdbd77f277
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2023-04-07 15:55:52 +0200

    feat: implement import functionality

--- swen2-template/src/main/java/at/fhtw/swen2/tutorial/presentation/view/ApplicationController.java
+++ swen2-template/src/main/java/at/fhtw/swen2/tutorial/presentation/view/ApplicationController.java
@@ -1,13 +1,21 @@
 package at.fhtw.swen2.tutorial.presentation.view;
 
+import at.fhtw.swen2.tutorial.model.Tour;
+import at.fhtw.swen2.tutorial.model.TourLog;
 import at.fhtw.swen2.tutorial.presentation.StageAware;
 import at.fhtw.swen2.tutorial.presentation.event.ApplicationShutdownEvent;
-import at.fhtw.swen2.tutorial.presentation.view.AboutDialogController;
+import at.fhtw.swen2.tutorial.presentation.viewmodel.TourListViewModel;
+import at.fhtw.swen2.tutorial.service.TourLogService;
+import at.fhtw.swen2.tutorial.service.TourService;
+import at.fhtw.swen2.tutorial.util.DataExportDTO;
 import at.fhtw.swen2.tutorial.util.DataIOUtil;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
@@ -15,6 +23,7 @@ import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -23,6 +32,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.util.List;
 import java.util.ResourceBundle;
 
 @Component
@@ -32,20 +43,36 @@ public class ApplicationController implements Initializable, StageAware {
 
     ApplicationEventPublisher publisher;
 
-    @FXML BorderPane layout;
+    @FXML
+    BorderPane layout;
 
     // Menu, at some point break out
-    @FXML MenuItem miPreferences;
-    @FXML MenuItem miQuit;
-    @FXML MenuItem miAbout;
-    @FXML MenuItem miExport;
-    @FXML MenuItem miImport;
+    @FXML
+    MenuItem miPreferences;
+    @FXML
+    MenuItem miQuit;
+    @FXML
+    MenuItem miAbout;
+    @FXML
+    MenuItem miExport;
+    @FXML
+    MenuItem miImport;
 
     // Toolbar, at some point break out
-    @FXML Label tbMonitorStatus;
+    @FXML
+    Label tbMonitorStatus;
     Circle monitorStatusIcon = new Circle(8);
 
     SimpleObjectProperty<Stage> stage = new SimpleObjectProperty<>();
+
+    @Autowired
+    TourService tourService;
+
+    @Autowired
+    TourLogService tourLogService;
+
+    @Autowired
+    private TourListViewModel tourListViewModel;
 
     public ApplicationController(ApplicationEventPublisher publisher) {
         log.debug("Initializing application controller");
@@ -73,7 +100,40 @@ public class ApplicationController implements Initializable, StageAware {
         this.stage.setValue(stage);
     }
 
+
+    // TODO: is this the right place for this?
     public void onFileImport(ActionEvent actionEvent) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save File");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("JSON Files", "*.json"));
+
+        // Show save file dialog
+        File file = fileChooser.showOpenDialog(stage.getValue());
+
+        if (file != null) {
+            // User selected a file, do something with it
+            try {
+                String jsonData = Files.readString(file.toPath());
+                List<DataExportDTO> data = objectMapper.readValue(jsonData, new TypeReference<>() {
+                });
+                // Loop through each exported data item and create a Callable for it
+                for (DataExportDTO dataItem : data) {
+                    Tour importedTour = tourService.saveTour(dataItem.getTour());
+                    tourListViewModel.addItem(importedTour);
+                    // Loop through each tour log in the exported data item and import it
+                    for (TourLog tourLog : dataItem.getTourLogs()) {
+                        tourLogService.saveTourLog(importedTour.getId(), tourLog);
+                    }
+                }
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setHeaderText("Error reading file");
+                alert.setContentText(e.getMessage());
+                alert.showAndWait();
+            }
+        }
     }
 
     // TODO: is this the right place for this?
